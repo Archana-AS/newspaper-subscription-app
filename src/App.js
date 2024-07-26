c059b554ced0abf2a076f4fb55ea68f4adfd3388
Archana-AS <archanaas2321@gmail.com>
2024-07-26 23:55:39 +0530

    Delete Subscriber created

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import NavBar from './components/NavBar';
 import AddSubscription from './components/AddSubscription';
 import View from './components/View';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div >
-     <Search/>
+     <AddSubscription/>
     </div>
   );
 }
